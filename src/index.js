77d5c32bb963092806aaf3e8694372062f3547ab
britt <britt.shroyer@gmail.com>
2019-05-09 23:12:38 -0500

    add dependencies & initial setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink, split } from 'apollo-client-preset';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
+import App from './App';
+
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
